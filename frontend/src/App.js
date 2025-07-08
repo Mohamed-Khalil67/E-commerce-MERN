ba0caea6c61df4cc706eb438c9e1a91c9033fc37
Mohamed Khalil <mohamed.khalil.ucbl@gmail.com>
2025-07-08 20:32:27 +0300

    Mongo db connection + database connection in backend, still a problme in showing out of stock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-
+import SigninScreen from './screens/SigninScreen';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import Badge from 'react-bootstrap/esm/Badge';
 import { Store } from './Store';
 import { useContext } from 'react';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -28,7 +29,7 @@ function App() {
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                      {cart.cartItems.length}
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
@@ -40,6 +41,8 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,9 +2,33 @@ import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Rating from './Rating';
+import axios from 'axios';
+import { useContext } from 'react';
+import { Store } from '../Store';
 
 function Product(props) {
   const { product } = props;
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+
   return (
     <Card className="product">
       <Link to={`/product/${product.slug}`}>
@@ -14,14 +38,20 @@ function Product(props) {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
-      </Card.Body>
-      <div className="product-info">
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>
           <strong>${product.price}</strong>
         </Card.Text>
-        <Button>Add to cart</Button>
-      </div>
+        {/* Debugging log - Check the value in browser's console */}
+        {console.log('countInStock:', product.countInStock)}
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out of Stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
+        )}
+      </Card.Body>
     </Card>
   );
 }
